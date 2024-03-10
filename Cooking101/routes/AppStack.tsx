4d64297c8d1c0dac59f96650bbdbf99e42966bbd
LeroyBG <leroylightning@g.ucla.edu>
2024-03-10 00:10:12 -0800

    add bottom tab navigation for completed screen

--- Cooking101/routes/AppStack.tsx
+++ Cooking101/routes/AppStack.tsx
@@ -1,20 +1,49 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
 import Home  from '../components/homepage/home'
 import { RecipeOverview } from "../components/RecipeOverview/RecipeOverview";
 import RecipeForm from "../components/uploadPage/upload";
 import stepsPage from '../components/steps/steps';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Stack = createStackNavigator()
+import { Image } from 'react-native'
+
+// import icons
+import HomeIcon from '../assets/icons/home.png'
+import HeartIcon from '../assets/icons/heart.png'
+import AddIcon from '../assets/icons/add.png'
+import ChefIcon from '../assets/icons/chef.png'
+
+const Tab = createBottomTabNavigator()
 
 // navigation stack for authenticated users
 export function AppStack() {
     return (
-        <Stack.Navigator>
-          <Stack.Screen name="homepage" component={Home} options={{headerShown: false}}/> 
-          <Stack.Screen name='RecipeOverview' component={RecipeOverview} options={{headerShown: false}} />
-          <Stack.Screen name="RecipeUpload" component={RecipeForm} options={{headerShown: false}} />
-          <Stack.Screen name="Steps" component={stepsPage} />
-        </Stack.Navigator>
+        <Tab.Navigator
+        screenOptions={{
+          tabBarLabelStyle: {
+            fontFamily: "PatrickHandSC-Regular",
+            fontSize: 15
+          }
+        }}>
+          {/* Shown in bottom tabs */}
+          <Tab.Screen name="homepage" component={Home} 
+          options={{
+            headerShown: false,
+            tabBarLabel: "Home",
+            tabBarIcon: ({size, focused, color}) => <Image source={HomeIcon} style={{ width: 35, height: 35 }} />,
+
+            }}/> 
+          <Tab.Screen name="RecipeUpload" component={RecipeForm} options={{
+            headerShown: false,
+            tabBarLabel: "Upload",
+            tabBarIcon: ({size, focused, color}) => <Image source={AddIcon} style={{ width: 42, height: 42 }} />,
+            }} />
+
+
+          {/* not shown in bottom tabs */}
+          <Tab.Screen name="Steps" component={stepsPage} options={{headerShown: false, tabBarButton: () => null}} />
+          <Tab.Screen name='RecipeOverview' component={RecipeOverview} options={{headerShown: false, tabBarButton: () => null}}/>
+
+        </Tab.Navigator>
     )
 }
